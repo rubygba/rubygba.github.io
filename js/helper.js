b70acec06690a89c54eb1fc99c313e604883f8bd
gameboyA <rubygba@foxmail.com>
2016-02-23 10:05:32 +0800

    day1
    1

--- js/helper.js
+++ js/helper.js
@@ -49,9 +49,17 @@ var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
+// 大写last name
+function inName(name) {
+  name = name.trim.split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+  return name[0] +" "+ name[1];
+}
+$("#main").append(internationalizeButton);
+
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,3 @@
-var role = "Web Developer";
-
-var formattedRole = HTMLheaderRole.replace("%data%", name); //自定义Role规格化文本
-var formattedName = HTMLheaderName.replace("%data%", role);
-$("#header").prepend(formattedRole); //jQ插入规格化文本
-$("#header").prepend(formattedName);
-
 var bio = {
   "name": "Game Boy A",
   "role": "Web Developer",
@@ -42,3 +35,71 @@ var education = {
     }
   ]
 }
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Udacity",
+      "title": "Course Developer",
+      "location": "Mountain View, CA",
+      "dates": "Feb 2014 - Current",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LearnBIG",
+      "title": "Software Engineer",
+      "location": "Seattle, WA",
+      "dates": "May 2013 - Jan 2014",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LEAD Academy Charter High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jul 2012 - May 2013",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Stratford High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jun 2009 - Jun 2012",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+};
+
+var role = "Web Developer";
+
+var formattedRole = HTMLheaderRole.replace("%data%", name); //自定义Role规格化文本
+var formattedName = HTMLheaderName.replace("%data%", role);
+$("#header").prepend(formattedRole); //jQ插入规格化文本
+$("#header").prepend(formattedName);
+
+for (job in work.jobs) {
+  // create new div for work experience
+  $("#workExperience").append(HTMLworkStart);
+  // concat employer and title
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+  $(".work-entry:last").append(formattedEmployerTitle);
+
+  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+  $(".work-entry:last").append(formattedDates);
+
+  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  $(".work-entry:last").append(formattedDescription);
+}
+
+function locationizer(work_obj) {
+    var locationArray = [];
+    for (job in work_obj.jobs) {
+        var newLocation = work_obj.jobs[job].location;
+        locationArray.push(newLocation);
+    }
+  return locationArray;
+}
+
+// Did locationizer() work? This line will tell you!
+console.log(locationizer(work));
