e728d3b9a93215a723669add96fb58c1c25d2dca
gameboyA <rubygba@foxmail.com>
2016-02-27 23:00:08 +0800

    bug fix
    downloadable

--- js/meme.js
+++ js/meme.js
@@ -8,24 +8,38 @@ function textChangeListener(evt) {
     window.bottomText = text;
   }
 
-  reDrawMeme(wind1.imageSrc, window.topText, window.bottomText);
+  reDrawMeme(window.imageSrc, window.topText, window.bottomText);
 }
 
 function reDrawMeme(image, top, bottom) {
   var c = document.querySelector('canvas');
   var ctx = c.getContext('2d');
   // draw loop
-  // TODO:
   if (image !== null)
     ctx.drawImage(image, 0, 0, c.width, c.height);
 
+  ctx.font = "36pt Impact";
+  ctx.textAlign = "center";
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 3;
+  if (top !== null) {
+  ctx.fillText(top, c.width/2, 40);
+  ctx.strokeText(top, c.width/2, 40);
+}
+  if (bottom !== null) {
+  ctx.fillText(bottom, c.width/2, c.height);
+  ctx.strokeText(bottom, c.width/2, c.height);
+}
+
+  // 缺角方形
   ctx.fillRect(0, 0, 50, 50);
   ctx.clearRect(0,0,25,25);
-
+  // 绿方框
   ctx.strokeStyle = "green";
   ctx.lineWidth = 2;
   ctx.strokeRect(100, 100, 100, 100);
-
+  // 蓝三角
   ctx.beginPath();
   ctx.moveTo(75, 75); // start
   ctx.lineTo(125, 75);
@@ -33,15 +47,6 @@ function reDrawMeme(image, top, bottom) {
   ctx.lineTo(75, 75); // end
   ctx.fillStyle = "blue";
   ctx.fill();
-
-  ctx.font = "36pt Impact";
-  ctx.textAlign = "center";
-  ctx.fillStyle = "white";
-  ctx.fillText("表情包 MEMES!", c.width/2, 40);
-
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 3;
-  ctx.strokeText("表情包 MEMES!", c.width/2, 40);
 }
 
 //var image = new Image();
@@ -53,18 +58,6 @@ image.onload = function() {
   // window.open(imageUrl);
   ctx.fillRect(0, 0, 50, 50);
   ctx.clearRect(0,0,25,25);
-
-  ctx.strokeStyle = "green";
-  ctx.lineWidth = 2;
-  ctx.strokeRect(100, 100, 100, 100);
-
-  ctx.beginPath();
-  ctx.moveTo(75, 75); // start
-  ctx.lineTo(125, 75);
-  ctx.lineTo(125, 125);
-  ctx.lineTo(75, 75); // end
-  ctx.fillStyle = "blue";
-  ctx.fill();
 
   ctx.font = "36pt Impact";
   ctx.textAlign = "center";
@@ -79,7 +72,7 @@ image.onload = function() {
 //image.src = "images/fry.jpg";
 
 function saveFile() {
-  window.open(document.querySelector('canvas').totoDataURL()); // ??
+  window.open(document.querySelector('canvas').toDataURL()); // ??
 }
 
 // ??????
